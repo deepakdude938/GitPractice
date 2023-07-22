c868e395a9a1491af5d90165f734c41c8d9e67f6
Deepak <deepak_vtest@appveen.com>
2023-07-22 21:45:17 +0530

    Second commit

---
+++ src/main/java/git/practice/HomePage.java
@@ -0,0 +1,10 @@
+package git.practice;
+
+public class HomePage {
+
+	public static void main(String[] args) {
+		System.out.println("Line 2");
+
+	}
+
+}
---
+++ src/main/java/git/practice/LoginPage.java
@@ -0,0 +1,12 @@
+package git.practice;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+	System.out.println("line 1");
+	System.out.println("line 2");
+
+
+	}
+
+}
